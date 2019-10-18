2750446b9ed01e6e09d8d0a1c746c4ad83ec4c3b
Tom Lloyd <tomlloyd1992@gmail.com>
2019-10-18 22:17:13 +0100

    Added Gun using ray casting to delete cylinder

---
+++ 3D FPS/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void takeDamage(float damage)
+    {
+        health -= damage;
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ 3D FPS/Assets/Scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    //Gun Stats
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera cam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+
+    void shoot()
+    {
+        RaycastHit hit;
+        if(        Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            EnemyScript target = hit.transform.GetComponent<EnemyScript>();
+
+            if(target != null)
+            {
+                target.takeDamage(damage);
+            }
+        }
+    }
+}
